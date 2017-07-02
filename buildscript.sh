2a84620f03f618cb10424ad45194b3277cdf5c8a
Jorge_de_la_Torre <jorgedlt@gmail.com>
2017-07-02 11:02:59 -0400

    Commit message

--- buildscript.sh
+++ buildscript.sh
@@ -11,9 +11,9 @@
 #
 
 # export AWS_DEFAULT_REGION=us-east-1    ; export ec2_ami="ami-2ef48339"    # us-east-1
-# export AWS_DEFAULT_REGION=us-east-2  ; export ec2_ami="ami-70edb615"      # us-east-2
+  export AWS_DEFAULT_REGION=us-east-2  ; export ec2_ami="ami-70edb615"      # us-east-2
 # export AWS_DEFAULT_REGION=us-west-1  ; export ec2_ami="ami-a9a8e4c9"      # us-west-1
-export AWS_DEFAULT_REGION=us-west-2  ; export ec2_ami="ami-746aba14"        # us-west-2
+# export AWS_DEFAULT_REGION=us-west-2  ; export ec2_ami="ami-746aba14"      # us-west-2
 # ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20160907 ( ebs | hvm )
 
 export vpc_stack="vpc-breakup"
@@ -25,7 +25,7 @@ export prv_cidr="10.27.1.0/24"
 export ec2_stack="breakup"
 
 # for Test I used SBD-DA AWS Account - us-east-2
-export ec2_keyname="sbdda-autodeploy-use2.pem"  # us-east-2
+export ec2_keyname="sbdda-autodeploy-use2.pem"                              # us-east-2
 #
 
 # t2.micro | t2.small | t2.medium | t2.large #
@@ -108,8 +108,6 @@ echo SecurityGroup Values >> ${VpcId}-build.log
 
  aws ec2 describe-security-groups --group-ids ${securitygroup} >> ${VpcId}-build.log
 
-exit
-
 sleep 10
 # Public EC2
 echo "Create EC2-Public"
@@ -123,6 +121,7 @@ InstancePu=$(aws ec2 run-instances \
   --subnet-id ${publicSubnet} \
   --associate-public-ip-address \
   --block-device-mappings "[{\"DeviceName\": \"/dev/sda1\",\"Ebs\":{\"VolumeSize\":16}}]" \
+  --user-data file://ec2configs/public-ec2-build.sh \
   --placement AvailabilityZone=${AvailabilityZone} >> ${VpcId}.public-build.log \
  | grep InstanceId | cut -d':' -f2 | tr -d '"|,| ' )
 
@@ -143,6 +142,7 @@ InstancePr=$(aws ec2 run-instances \
   --security-group-ids ${securitygroup} \
   --subnet-id ${privateSubnet} \
   --block-device-mappings "[{\"DeviceName\": \"/dev/sda1\",\"Ebs\":{\"VolumeSize\":16}}]" \
+  --user-data file://ec2configs/private-ec2-build.sh \
   --placement AvailabilityZone=${AvailabilityZone} >> ${VpcId}.private-build.log \
  | grep InstanceId | cut -d':' -f2 | tr -d '"|,| ' )
 
